b9b02e6c5b24dd8536fc23e04c5371f8a9ef5996
Melissa <melpatenator@gmail.com>
2015-12-07 23:00:56 -0600

    add json file, views, and controller to get a basic handle on where this app is going

--- app/spent/spent.js
+++ app/spent/spent.js
@@ -1,14 +1,24 @@
 'use strict';
 
-angular.module('myApp.spent', ['ngRoute'])
+var privilegeApp = angular.module('myApp.spent', ['ngRoute']);
 
-.config(['$routeProvider', function($routeProvider) {
+privilegeApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/spent', {
     templateUrl: 'spent/spent.html',
     controller: 'SpentCtrl'
   });
 }])
 
-.controller('SpentCtrl', [function() {
 
+privilegeApp.controller('SpentCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.dateTime = Date();
+  $http.get('/app/money_spent/money_spent.json').then(function(data) {
+    $scope.spenditures = data.data;
+    console.log('spenditures', $scope.spenditures);
+  });
+
+  $scope.saveData = function () {
+    window.localStorage['moneySpent'] = angular.toJson(spending);
+    $scope.output = 'Ok, got it: ' + JSON.stringify($scope.spenditures);
+    };
 }]);
